dc7f676e1e5da25aff0c56496d5e813dad5948f4
wrritam <bruceritamwayne02@gmail.com>
2025-04-09 16:21:39 +0530

    extended authentication added

---
+++ src/controllers/auth/reset-password.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import prisma from "../../db/db.config";
+import bcrypt from "bcrypt";
+
+export const resetPassword = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const { password, email } = req.body;
+  const hashedPassword: string = await new Promise((resolve, reject) => {
+    bcrypt.hash(password, 7, (err, hash) => {
+      if (err) reject(err);
+      else resolve(hash);
+    });
+  });
+  if (hashedPassword) {
+    await prisma.user.update({
+      where: { email: email },
+      data: {
+        password: hashedPassword,
+        updatedAt: new Date().toISOString(),
+      },
+    });
+    res.json({ message: "Password updated successfully" });
+  } else {
+    res.json({ message: "Could not update password" });
+  }
+};
