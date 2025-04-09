fabf781ada6dfba4271cfc50694cc1746ad7418f
wrritam <bruceritamwayne02@gmail.com>
2025-04-09 16:13:35 +0530

    few things added in authentication

---
+++ prisma/migrations/20250409103158_init/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `is_verified` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "is_verified" BOOLEAN NOT NULL,
+ADD COLUMN     "last_login" TEXT,
+ADD COLUMN     "otp" INTEGER,
+ADD COLUMN     "password" TEXT NOT NULL;
