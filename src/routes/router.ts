0f3e13ef2a1df2cb297b755160f0f6f6aeb1d16c
wrritam <bruceritamwayne02@gmail.com>
2025-04-11 00:03:36 +0530

    deleteRequest added and tested

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,7 +3,9 @@ const router = express.Router();
 
 import { runRequest } from "../controllers/runRequest";
 import { authentication } from "../middleware/authenticator";
+import { deleteRequest } from "../controllers/deleteRequest";
 
 router.post("/run-request", authentication, runRequest);
+router.delete("/delete-request/:requestId", authentication, deleteRequest);
 
 export default router;
