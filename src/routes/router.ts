bf5a380a20575b165663b15621617a9e8c1ac58e
wrritam <bruceritamwayne02@gmail.com>
2025-04-16 21:16:14 +0530

    removing the debugging bug and lil changes in export

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,12 +4,10 @@ const router = express.Router();
 import { runRequest } from "../controllers/runRequest";
 import { authentication } from "../middleware/authenticator";
 import { deleteRequest } from "../controllers/deleteRequest";
-import { groupedInsights } from "../controllers/getGroupedInsights";
-
-console.log(groupedInsights);
+import { allInsights } from "../controllers/getGroupedInsights";
 
 router.post("/run-request", authentication, runRequest);
 router.delete("/delete-request/:requestId", authentication, deleteRequest);
-router.get("/all-insights", authentication, groupedInsights);
+router.get("/all-insights", authentication, allInsights);
 
 export default router;
