8b542479ebe19f00b8d5c7e27004cd2baaa0dd18
wrritam <bruceritamwayne02@gmail.com>
2025-05-26 09:31:46 +0530

    a bit change in the rate limiter path

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,20 +1,20 @@
-import express from "express";
-const authRouter = express.Router();
+import express from 'express'
+const authRouter = express.Router()
 
-import { rateLimiterforAuth } from "../services/rate-limiter";
-import { register } from "../controllers/auth/register";
-import { verifyRegistration } from "../controllers/auth/verify-registration";
-import { login } from "../controllers/auth/login";
-import { forgotPassword } from "../controllers/auth/forgot-password";
-import { resetPassword } from "../controllers/auth/reset-password";
-import { verifyUpdation } from "../controllers/auth/verify-update";
+import { rateLimiterforAuth } from '../middleware/rate-limiter'
+import { register } from '../controllers/auth/register'
+import { verifyRegistration } from '../controllers/auth/verify-registration'
+import { login } from '../controllers/auth/login'
+import { forgotPassword } from '../controllers/auth/forgot-password'
+import { resetPassword } from '../controllers/auth/reset-password'
+import { verifyUpdation } from '../controllers/auth/verify-update'
 
-authRouter.use(rateLimiterforAuth);
-authRouter.post("/register", register);
-authRouter.post("/verify-registration", verifyRegistration);
-authRouter.post("/login", login);
-authRouter.post("/forgot-password", forgotPassword);
-authRouter.post("/verify-updation", verifyUpdation);
-authRouter.post("/reset-password", resetPassword);
+authRouter.use(rateLimiterforAuth)
+authRouter.post('/register', register)
+authRouter.post('/verify-registration', verifyRegistration)
+authRouter.post('/login', login)
+authRouter.post('/forgot-password', forgotPassword)
+authRouter.post('/verify-updation', verifyUpdation)
+authRouter.post('/reset-password', resetPassword)
 
-export default authRouter;
+export default authRouter
--- apps/backend/src/routes/main.ts
+++ apps/backend/src/routes/main.ts
@@ -1,28 +1,28 @@
-import express from "express";
-const router = express.Router();
+import express from 'express'
+const router = express.Router()
 
-import { rateLimiterforAPIs } from "../services/rate-limiter";
+import { rateLimiterforAPIs } from '../middleware/rate-limiter'
 import {
   validateRunRequest,
   validateSearchLogs,
   validateDeleteRequest,
   validateAllInsights,
-} from "../middleware/request-validator";
-import { runRequest } from "../controllers/main/run-request";
-import { authentication } from "../middleware/authenticator";
-import { deleteRequest } from "../controllers/main/delete-request";
-import { allInsights } from "../controllers/main/get-grouped-insights";
-import { searchLogs } from "../controllers/main/search-logs";
+} from '../middleware/request-validator'
+import { runRequest } from '../controllers/main/run-request'
+import { authentication } from '../middleware/authenticator'
+import { deleteRequest } from '../controllers/main/delete-request'
+import { allInsights } from '../controllers/main/get-grouped-insights'
+import { searchLogs } from '../controllers/main/search-logs'
 
-router.use(rateLimiterforAPIs);
-router.post("/run-request", authentication, validateRunRequest, runRequest);
+router.use(rateLimiterforAPIs)
+router.post('/run-request', authentication, validateRunRequest, runRequest)
 router.delete(
-  "/delete-request/:requestId",
+  '/delete-request/:requestId',
   authentication,
   validateDeleteRequest,
   deleteRequest
-);
-router.get("/search-logs", authentication, validateSearchLogs, searchLogs);
-router.get("/all-insights", validateAllInsights, authentication, allInsights);
+)
+router.get('/search-logs', authentication, validateSearchLogs, searchLogs)
+router.get('/all-insights', validateAllInsights, authentication, allInsights)
 
-export default router;
+export default router
